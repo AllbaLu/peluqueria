3915604e7b05da27f4df16f68ff92d7e3d62b21d
albal <albaluccia@gmail.com>
2025-04-04 15:33:18 +0800

    changes to formContact

--- src/front/component/about.jsx
+++ src/front/component/about.jsx
@@ -11,16 +11,14 @@ export const About = () => {
 
             <div className="card bg-transparent text-white" style={{width: "70rem"}}>
                 <div className="card-body ">
-                    <h1 className="card-title">About</h1>
-                    <h6 className="card-subtitle mb-2 text-muted">Card subtitle</h6>
-                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatum tempore,
-                        iusto dolore adipisci eligendi repudiandae labore nesciunt nihil, quas reprehenderit vero corrupti accusantium fugiat, quasi ut. 
-                        Cumque ut a quibusdam?</p>
-                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aspernatur sunt similique cupiditate velit magnam consectetur aut ex? 
-                        Placeat voluptatem et quibusdam, blanditiis voluptas hic quaerat illum aliquam quo porro distinctio.</p>
-                    <a href="#" className="card-link">Card link</a>
-                    <a href="#" className="card-link">Another link</a>
+                    <h1 className="card-title">Sobre mí</h1>
+                    <h5 className="card-subtitle mb-2 mt-3 text-muted"><em><strong>Experiencia y Pasión por la Belleza Capilar</strong></em> </h5>
+                    <p className="card-text" style={{textAlign: "justify"}}>
+                    Con más de 40 años de trayectoria en el mundo de la peluquería, 
+                    mi misión es realzar la belleza y personalidad de cada cliente a través de técnicas personalizadas y las últimas tendencias. 
+                    Mi experiencia me permite trabajar con todo tipo de cabello y estilos, brindando un servicio profesional y adaptado a cada necesidad.</p>
+                    
+                    
                 </div>
             </div>
         </section>
--- src/front/component/services.jsx
+++ src/front/component/services.jsx
@@ -7,15 +7,35 @@ export const Services = () => {
     
     return(
         <section id="services">
+            
+            <div className="container">
+                <div className="title">
 
-            <div>
-                <h1 className="text-white">
-                    Services
-                </h1>
-                <div className="d-flex justify-content-center ">
-                    <ul className="list-gropu text-white">
-                        <li>serv 1</li>
-                        <li>serv 2</li>
+                    <h1 className="text-white">
+                        Services
+                    </h1>
+                </div>
+                <div className="d-flex justify-content-evenly ">
+                    <ul className="list text-warning">
+                        <li className="list">
+                            <i className="fa-light fa-scissors" style={{color: "#FFD43B"}}></i><strong></strong>
+                                <p>
+                                    <a className="btn btn-primary" data-bs-toggle="collapse" href="#collapseExample" role="button" aria-expanded="false" aria-controls="collapseExample">
+                                        Corte de Cabello
+                                    </a>
+                                    
+                                </p>
+                                    <div className="collapse" id="collapseExample">
+                                    <div className="card card-body">
+                                        Desde cortes clásicos hasta los más modernos y personalizados para mujeres, hombres y niños.
+                                    </div>
+                                    </div>
+                            
+                        </li>
+                        <li className="list">
+                           
+                            
+                        </li>
                         <li>serv 3</li>
                     </ul>
                     <ul className="list-gropu text-white">
